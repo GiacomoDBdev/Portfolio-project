f9967544bd81b2222d4495ad6bc0969e5311da80
Giacomo Di Biagio <116678481+GiacomoDBdev@users.noreply.github.com>
2024-02-22 10:25:01 +0100

    Add all files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    controlArrows: false,
+    sectionsColor: [ '#6C2EF2','#6E43F0','#6E56F0','#7663F2'],
+    navigation: true,
+    navigationTooltips: ['About Me', 'Projects', 'Tools', 'Keep in Touch'],
+    slidesNavigation:true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+    menu: '#Menu',
+    loopBottom: false,
+    
+
+
+});
+
+
+function openMenu()
+{
+    document.getElementById('container').style.right='0%';
+    document.getElementById('Menu').style.opacity='1';
+};
+
+function closeMenu()
+{   document.getElementById('container').style.right='-100%';
+    document.getElementById('Menu').style.opacity='0';
+}
